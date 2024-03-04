a3c59bc178833117cf53aa6c08b3afd040f6b4be
Vadim Kovalev <nikas.367@mail.ru>
2024-03-05 00:32:24 +0300

    src

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header/Header";
+import Slider from "./components/slider/Slider";
+import Promo from "./components/promo/Promo";
+import SliderSnowbord from "./components/sliderSnowbord/SliderSnowbord";
+
+import './styles/reset.css'
+import './styles/common.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Slider />
+      <Promo />
+      <SliderSnowbord />
+    </>
   );
 }
 
